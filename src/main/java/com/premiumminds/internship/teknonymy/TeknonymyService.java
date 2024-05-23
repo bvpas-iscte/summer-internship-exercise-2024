2741d0cb5066339d29c3a67fd615282e532f1858
bvpas-iscte <160756976+bvpas-iscte@users.noreply.github.com>
2024-05-23 14:02:06 +0100

    basic code structure and idea of implementation

--- src/main/java/com/premiumminds/internship/teknonymy/TeknonymyService.java
+++ src/main/java/com/premiumminds/internship/teknonymy/TeknonymyService.java
@@ -13,4 +13,12 @@ class TeknonymyService implements ITeknonymyService {
   public String getTeknonymy(Person person) {
     throw new RuntimeException("Not Implemented Yet");
   };
+
+  //method to determine if a person has descendants (boolean)
+
+  //method with DPS algorithm to determine the deepest and oldest child
+
+  //method to build teknonymy based on previous method 
+
+  //maybe an auxiliar private class that stores the generation of children -> for recursive and comparison implementations
 }
